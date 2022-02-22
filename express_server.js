8333e347d760a8f5f101fcc85600073912506f01
trijaychan <your@email.com>
2022-02-22 03:16:42 +0000

    added a second route and a template

--- express_server.js
+++ express_server.js
@@ -7,7 +7,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.set("view-engine", "ejs");
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -19,8 +19,18 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
 })
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
-})
+});
