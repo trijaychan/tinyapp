4205cfa38e0c0424d5d504c8393ba797c096060c
trijaychan <your@email.com>
2022-02-25 10:34:16 +0000

    added comments

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,7 +1,9 @@
-const { assert } = require('chai');
+// helpersTeest.js
 
+const { assert } = require('chai');
 const { getUserByKey } = require('../helpers.js');
 
+// a fake user database for testing
 const testUsers = {
   "userRandomID": {
     id: "userRandomID",
@@ -15,13 +17,16 @@ const testUsers = {
   }
 };
 
-describe('getUserByEmail', function() {
+// getUserByKey function
+describe('getUserByKey', function() {
+  // testing function with a valid email
   it('should return a user with valid email', function() {
     const user = getUserByKey("email", "user@example.com", testUsers);
     const expectedUserID = "userRandomID";
     assert.strictEqual(expectedUserID, user);
   });
 
+  // testing function with an invalid email
   it('should return undefined', function() {
     const user = getUserByKey("email", "user3@example.com", testUsers);
     const expectedUserID = undefined;
